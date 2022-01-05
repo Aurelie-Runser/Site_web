4519f42cbea2f1372e4ed41f541ed973f8021ffd
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2022-01-05 01:45:46 +0100

    js - mise en place du carrousel sur la page article "la muette"

--- src/js/fonctions.js
+++ src/js/fonctions.js
@@ -1,4 +1,4 @@
-function carrouselF() {
+function carrouselFilms() {
     
 	var imagesF = document.querySelectorAll('.imageF');
     let d = 2000;
@@ -15,6 +15,44 @@ function carrouselF() {
 
     animF.add({
         targets: imagesF,
+        translateX: [{value: '319', duration: d},
+                     {value: '-319', duration: 0, delay: function(img,ind) {
+                         if (ind == 0) return (3*delta)+(2*d);
+                         if (ind == 1) return (2*delta)+d;
+                         if (ind == 2) return delta;
+                         return 0;
+                     }},
+                     {value: '0', duration: function(img,ind) {
+                         if (ind == 3) return 0;
+                         return d;
+                     }}],
+        easing: 'linear',
+        delay: function(img,ind) {
+            if (ind == 0) return delta;
+            if (ind == 1) return (2*delta)+d;
+            if (ind == 2) return (3*delta)+(2*d);
+            return (4*delta)+(3*d);
+        }
+    });
+}
+
+function carrouselLivres() {
+    
+	var imagesL = document.querySelectorAll('.imageL');
+    let d = 2000;
+    let delta = 1000;
+
+    imagesL.forEach(function(img,indice) {
+        img.style.zIndex = imagesL.length - indice;
+    });
+
+    let animL = anime.timeline({
+        loop: true,
+        delay: 0
+    });
+
+    animL.add({
+        targets: imagesL,
         translateX: [{value: '319', duration: d},
                      {value: '-319', duration: 0, delay: function(img,ind) {
                          if (ind == 0) return (3*delta)+(2*d);
